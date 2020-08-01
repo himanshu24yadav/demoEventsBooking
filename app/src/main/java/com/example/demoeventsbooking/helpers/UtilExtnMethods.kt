902763c0ec9e4695c43747605f57d6b4e39b1827
Himanshu Yadav <himanshu24yadav@gmail.com>
2020-08-02 02:49:36 +0530

    MainActivity UI

--- app/src/main/java/com/example/demoeventsbooking/helpers/UtilExtnMethods.kt
+++ app/src/main/java/com/example/demoeventsbooking/helpers/UtilExtnMethods.kt
@@ -5,7 +5,6 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 
-
 fun Context.isNetworkAvailable() : Boolean{
     val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
