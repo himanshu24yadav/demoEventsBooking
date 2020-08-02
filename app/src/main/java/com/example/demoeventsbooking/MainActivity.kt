a63a38b5c247959a48d0509721ccea200bd19324
Himanshu Yadav <himanshu24yadav@gmail.com>
2020-08-03 03:19:33 +0530

    LocationFeature Added

--- app/src/main/java/com/example/demoeventsbooking/MainActivity.kt
+++ app/src/main/java/com/example/demoeventsbooking/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.demoeventsbooking
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -10,7 +13,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.demoeventsbooking.databinding.ActivityMainBinding
 import com.example.demoeventsbooking.helpers.UtilConstants
-import com.example.demoeventsbooking.helpers.UtilMethods
 import com.example.demoeventsbooking.helpers.isNetworkAvailable
 import com.example.demoeventsbooking.homeSection.adapter.EventAdapter
 import com.example.demoeventsbooking.homeSection.dataManager.MasterListEventModel
@@ -21,13 +23,15 @@ const val VIEW_FOR_FAILURE = 200
 const val VIEW_FOR_NO_DATA = 300
 const val VIEW_FOR_NETWORK_NOT_AVAILABLE = 400
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
 
     private var mEventViewModel : ViewModelEventsList? = null
     private lateinit var mLayoutBinding: ActivityMainBinding
     private var mLinearLayoutManager: RecyclerView.LayoutManager? = null
     private var mEventAdapter:EventAdapter? = null
     private var mEventList:ArrayList<MasterListEventModel> = ArrayList()
+    private var mSelectedCity: String = UtilConstants.DEFAULT_CITY
+    private var mPopupMenu: PopupMenu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,15 +50,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun initListeners() {
         mLayoutBinding.retryTv.setOnClickListener {
-            if(isNetworkAvailable()) {
-                mLayoutBinding.errorGroup.visibility = View.GONE
-                mLayoutBinding.rvEvents.visibility = View.GONE
-                mLayoutBinding.progressGroup.visibility = View.VISIBLE
-                initFetchEventData()
-            } else {
-                UtilMethods.showToast(this,"No network")
-            }
+            initFetchEventData()
         }
+
+        mLayoutBinding.locationIv.setOnClickListener {
+            mPopupMenu?.show()
+        }
+
+        mPopupMenu?.setOnMenuItemClickListener(this@MainActivity)
     }
 
     private fun initView() {
@@ -63,11 +66,15 @@ class MainActivity : AppCompatActivity() {
             layoutManager = mLinearLayoutManager
             itemAnimator = null
         }
+        mPopupMenu?.inflate(R.menu.menu_locations)
+        mPopupMenu?.gravity = Gravity.END
+        mPopupMenu?.menu?.findItem(R.id.online_item)?.isChecked = true
     }
 
     private fun initObject() {
         mEventViewModel = ViewModelProvider(this).get(ViewModelEventsList::class.java)
         mLinearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        mPopupMenu =  PopupMenu(this@MainActivity, mLayoutBinding.locationIv)
     }
 
     private fun initObserver() {
@@ -93,7 +100,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun initFetchEventData() {
         if(isNetworkAvailable()) {
-            mEventViewModel?.fetchAllEvents()
+            mLayoutBinding.errorGroup.visibility = View.GONE
+            mLayoutBinding.rvEvents.visibility = View.GONE
+            mLayoutBinding.progressGroup.visibility = View.VISIBLE
+            mEventViewModel?.fetchAllEvents(mSelectedCity)
         } else {
             updateView(VIEW_FOR_NETWORK_NOT_AVAILABLE)
         }
@@ -133,5 +143,34 @@ class MainActivity : AppCompatActivity() {
             mEventAdapter = EventAdapter(this, mEventList)
             mLayoutBinding.rvEvents.adapter = mEventAdapter
         }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.online_item ->{
+                mSelectedCity = UtilConstants.DEFAULT_CITY
+            }
+            R.id.delhi_item ->{
+                mSelectedCity = UtilConstants.DELHI_CITY
+            }
+            R.id.lucknow_item ->{
+                mSelectedCity = UtilConstants.LUCKNOW_CITY
+            }
+            R.id.pune_item ->{
+                mSelectedCity = UtilConstants.PUNE_CITY
+            }
+            R.id.kolkata_item ->{
+                mSelectedCity = UtilConstants.KOLKATA_CITY
+            }
+            R.id.mumbai_item ->{
+                mSelectedCity = UtilConstants.MUMBAI_CITY
+            }
+            else -> {
+                return false
+            }
+        }
+        item.isChecked = true
+        initFetchEventData()
+        return true
     }
 }
--- app/src/main/java/com/example/demoeventsbooking/helpers/UtilConstants.kt
+++ app/src/main/java/com/example/demoeventsbooking/helpers/UtilConstants.kt
@@ -5,10 +5,15 @@ class UtilConstants {
         const val PARAM_NORM = "norm"
         const val PARAM_FILTER_BY = "filterBy"
         const val PARAM_CITY = "city"
-        const val DEFAULT_CITY = "delhi"
         const val STATUS_FAILURE = "failure"
         const val STATUS_SUCCESS = "success"
         const val STATUS_NO_DATA = "no_data"
+        const val DEFAULT_CITY = "online"
+        const val LUCKNOW_CITY = "lucknow"
+        const val MUMBAI_CITY = "mumbai"
+        const val DELHI_CITY = "delhi"
+        const val PUNE_CITY = "pune"
+        const val KOLKATA_CITY = "kolkata"
 
         const val API_CONSTANT_DEFAULT = 1000
     }
